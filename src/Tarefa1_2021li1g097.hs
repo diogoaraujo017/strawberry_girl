f28e83d5596fe2589929139a78d3ef615266d685
Diogo Araújo <a100544@alunos.uminho.pt>
2021-11-14 13:38:17 +0000

    Avanço na construção da função existeVazio no ficheiro Tarefa1_2021li1g097.hs.

--- src/Tarefa1_2021li1g097.hs
+++ src/Tarefa1_2021li1g097.hs
@@ -13,50 +13,58 @@ import LI12122
 import Language.Haskell.TH (charPrimL)
 import Generics.SYB (ConstrRep(CharConstr))
 
-{-Esta função testa se o mapa é valido utilizando as funções auxiliares que estão posteriormente defenidas.-}
+{-Esta função testa se o mapa é valido utilizando as funções auxiliares que estão posteriormente defenidas.-} --Done
 validaPotencialMapa :: [(Peca, Coordenadas)] -> Bool
 validaPotencialMapa [] = False
-validaPotencialMapa m = undefined
+validaPotencialMapa m = validaPosicao m && portaValida m && caixaValida m && existeVazio m && chaoContinuo m 
 
 
-{-Esta função testa se existem peças que têm as mesmas coordenadas.-}
+{-Esta função testa se existem peças que têm as mesmas coordenadas.-}  --Done
 validaPosicao :: [(Peca, Coordenadas)] -> Bool
 validaPosicao ((p1,(x1,y1)):[]) = True
 validaPosicao ((p1,(x1,y1)):(p2,(x2,y2)):t)
  |x1==x2 && y1==y2 = False
  |otherwise = validaPosicao ((p1,(x1,y1)):t) && validaPosicao ((p2,(x2,y2)):t)
 
-{-Esta função testa se existe apenas uma porta usando a função existePorta.-}
+{-Esta função testa se existe apenas uma porta usando a função existePorta.-}  --Done
 portaValida :: [(Peca, Coordenadas)] -> Bool
 portaValida m
   |existePorta m == 1 = True
   |otherwise = False
 
-{-Esta função testa quantas portas existem.-}
+{-Esta função testa quantas portas existem.-} --Done
 existePorta :: [(Peca, Coordenadas)] -> Int
 existePorta [] = 0
 existePorta ((Porta, (x,y)):t) = 1 + existePorta t
 existePorta ((_, (x,y)):t) = existePorta t
 
 
-{-Esta função testa se as caixas estão propriamente colocadas no mapa, dando o valor de False se existirem caixas a flutuar.-}
+{-Esta função testa se as caixas estão propriamente colocadas no mapa, dando o valor de False se existirem caixas a flutuar.-}  --Done
 caixaValida :: [(Peca, Coordenadas)] -> Bool
 caixaValida [] = True
 caixaValida ((Caixa, (x1,y1)):[]) = False
-caixaValida ((Caixa, (x1,y1)):(p, (x2,y2)):t) = case p of Bloco -> if x1 == x2 && y1 == (y2-1) then True else caixaValida ((Caixa, (x1,y1)):t)
+caixaValida ((Caixa, (x1,y1)):(p, (x2,y2)):t) = case p of Bloco -> (x1 == x2 && y1 == (y2-1)) || caixaValida ((Caixa, (x1,y1)):t)
                                                           Porta -> caixaValida ((Caixa, (x1,y1)):t)
-                                                          Caixa -> if x1 == x2 && y1 == (y2-1) then True && (caixaValida ((p, (x2,y2)):t)) else caixaValida ((Caixa, (x1,y1)):t)
-caixaValida ((_, (x,y)):t) = caixaValida t
+                                                          Caixa -> if x1 == x2 && y1 == (y2-1) then caixaValida ((p, (x2,y2)):t) else caixaValida ((Caixa, (x1,y1)):t)
+caixaValida ((p, (x,y)):t) = caixaValida t 
 
 
-{-Esta função testa se existe pelo menos um espaço vazio no mapa.-}
-existeVazio :: [(Peca, Coordenadas)] -> Bool
-existeVazio = undefined 
+{-Esta função testa se existe pelo menos um espaço vazio no mapa.-} --Por fazer
+{-existeVazio :: [(Peca, Coordenadas)] -> Bool
+existeVazio ((p1, (x1,y1)):[]) = False
+existeVazio ((p1, (x1,y1)):(p2, (x2,y2)):t) 
+  |p1 == Vazio || p2 == Vazio = True
+  |x1 == x2 && y1 == (y2+1) = existeVazio ((p2, (x2,y2)):t)
+  |x2 == (x1+1) = existeVazio ((p2,(x2,y2)):t)
+  |otherwise = True 
+-}
 
-{-Esta função testa se existe um chão continuo-}
+{-Esta função testa se existe um chão continuo-}  --Por fazer 
 chaoContinuo :: [(Peca, Coordenadas)] -> Bool 
 -- chaoContinuo [] = True ?????
 chaoContinuo = undefined
+
+
 
 
 
