46e7a439b2cd20255c28598d7714547f87af348d
Diogo Araújo <a100544@alunos.uminho.pt>
2021-12-17 08:54:48 +0000

    Criação de um ficheiro para guardar os Mapas.hs

---
+++ src/Mapas.hs
@@ -0,0 +1,24 @@
+module Mapas where
+
+player1 = undefined 
+player2 = undefined 
+player3 = undefined 
+player4 = undefined 
+player5 = undefined 
+player6 = undefined 
+player7 = undefined 
+player8 = undefined 
+player9 = undefined 
+player10 = undefined 
+
+level1 = undefined 
+level2 = undefined 
+level3 = undefined 
+level4 = undefined 
+level5 = undefined 
+level6 = undefined 
+level7 = undefined 
+level8 = undefined 
+level9 = undefined 
+level10 = undefined 
+
