258c417ccbfc45d7d4203c356987aba69482246c
a100649 <a100649@alunos.uminho.pt>
2022-01-05 11:45:47 +0000

    Avanços na Tarefa6_2021li1g097.hs

--- src/Tarefa6_2021li1g097.hs
+++ src/Tarefa6_2021li1g097.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {- |
 Module      : Tarefa6_2021li1g097
 Description : Resolução de um puzzle
@@ -17,9 +18,22 @@ import Tarefa3_2021li1g097 ()
 import Tarefa4_2021li1g097 () 
 
 resolveJogo :: Int -> Jogo -> Maybe [Movimento]
-resolveJogo i jogo = undefined
+--resolveJogo i jogo = undefined
+resolveJogo i (Jogo (p:ps) (Jogador (x,y) d b))
+ |i==0 = if x == fst (cordPorta (0,0) (Jogo (p:ps) (Jogador (x,y) d b))) && y == snd (cordPorta (0,0) (Jogo (p:ps) (Jogador (x,y) d b)))
+         then Just []
+         else Nothing
+resolveJogo i (Jogo (p:ps) (Jogador (x,y) d b)) = AndarEsquerda : resolveJogo i-1 (Jogo (p:ps) (Jogador (x-1,y) d b)) || AndarDireita : resolveJogo i-1 (Jogo (p:ps) (Jogador (x+1,y) d b)) || if d==oeste then Trepar : resolveJogo i-1 (Jogo (p:ps) (Jogador (x-1,y+1) d b)) else Trepar : resolveJogo i-1 (Jogo (p:ps) (Jogador (x+1,y+1) d b)) || InterageCaixa : resolveJogo i-1 (Jogo (p:ps) (Jogador (x,y) d b))
 
---Tarefa6 :
---Decision Tree
---Trie(Data Structure)
---Combinações
+
+cordPorta :: (Int,Int) -> Jogo -> (Int,Int)
+cordPorta (a1,a2) (Jogo (p:ps) (Jogador (x,y) d b)) = lPorta (0,0) p (Jogador (x,y) d b)
+                                                     where lPorta :: (Int,Int) -> [Peca] -> Jogador -> (Int,Int)
+                                                           lPorta (a1,a2) [] (Jogador (x,y) d b) = cordPorta (0,a2+1) (Jogo ps (Jogador (x,y) d b))
+                                                           lPorta (a1,a2) (h:t) (Jogador (x,y) d b) = case h of Porta -> (a1,a2)
+                                                                                                                Bloco -> lPorta (a1+1,a2) t (Jogador (x,y) d b)
+                                                                                                                Caixa -> lPorta (a1+1,a2) t (Jogador (x,y) d b)
+                                                                                                                Vazio -> lPorta (a1+1,a2) t (Jogador (x,y) d b)
+
+
+
